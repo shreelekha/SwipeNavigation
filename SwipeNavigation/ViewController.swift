f1e390977869b6a9522af5da7f1b5ad0792fd24b
shreelekha <shree.tanna@gmail.com>
2017-01-27 15:23:38 -0800

    Initial Commit

---
+++ SwipeNavigation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwipeNavigation
+//
+//  Created by Shree Tanna on 2017-01-27.
+//  Copyright © 2017 Shree. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
